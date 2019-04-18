4c708bba568fc915b862c12982926df20070f23a
lixs5 <lixs5@asiainfo.com>
2019-04-18 22:48:14 +0800

    更新备份

--- src/script/search.js
+++ src/script/search.js
@@ -4,31 +4,40 @@ define([
 ], function (require, data) {
   'use strict';
   class search {
-    constructor(data) {
+    constructor(data, option) {
+      this.option = option ? option : {}
       this.datainit(data)
     }
 
     search(v) {
+      console.time('搜索时间:')
       let results = []
       // 等同于使用map.entries()
       for (let [key, value] of this.queryArrayMap) {
-        if(value.match(v)){
+        if (value.match(v)) {
           results.push(key)
         }
       }
-
+      console.timeEnd('搜索时间:')
       return results
     }
 
     datainit(data) {
       let m = this.queryArrayMap = new Map()
+      let sks = this.option.searchKeys
       for (const key in data) {
         const el = data[key]
         for (let i = 0; i < el.length; i++) {
           const item = el[i].item
           for (let j = 0; j < item.length; j++) {
             const meta = item[j];
-            m.set(meta, JSON.stringify(meta)) 
+            let querystr = ''
+            let sks = this.option.searchKeys ? this.option.searchKeys : Object.keys(meta)
+            sks.forEach((k) => {
+              if (meta.hasOwnProperty(k))
+                querystr += " " + meta[k]
+            })
+            m.set(meta, querystr)
           }
         }
       }
