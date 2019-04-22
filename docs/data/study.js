9e9ca4996da1803fad18dd083bc02dd52b5d84c9
bingo <binhdu@foxmail.com>
2019-04-22 10:36:51 +0800

    数据更新

--- docs/data/study.js
+++ docs/data/study.js
@@ -4,38 +4,168 @@ define(['require'], function (require) {
   'use strict';
 
   return [{
-    "title": "综合",
+    "title": "社区",
     "icon": "icon-router",
     "item": [{
-      "url": "https://weibo.com/u/3816118370/home?topnav=1&wvr=6",
-      "icon": "https://weibo.com/favicon.ico",
-      "name": "微博",
-      "desc": "随时随地发现新鲜事"
-    }, {
-      "url": "https://github.com/",
-      "icon": "https://github.githubassets.com/favicon.ico",
-      "name": "github",
-      "desc": "开源代码库"
-    }, {
-      "url": "https://www.zhihu.com/",
-      "icon": "https://uploads.moulem.com/id0siteimg/z204.png",
-      "name": "知乎",
-      "desc": "有问题，上知乎"
-    }, {
-      "url": "https://www.huxiu.com/",
-      "icon": "https://www.huxiu.com/favicon.ico",
-      "name": "虎嗅",
-      "desc": "科技资讯,商业评论,明星公司,动态,宏观,趋势,创业,精选,有料,干货,有用,细节,内幕"
+      "url": "https://segmentfault.com/",
+      "icon": "https://segmentfault.com/favicon.ico",
+      "name": "SegmentFault思否",
+      "desc": "SegmentFault 思否 为开发者提供问答、学习与交流编程知识的平台，创造属于开发者的时代！"
     }, {
       "url": "https://juejin.im/timeline",
       "icon": "https://b-gold-cdn.xitu.io/favicons/v2/favicon.ico",
       "name": "掘金",
       "desc": "前端社区"
     }, {
-      "url": "http://www.nicotv.me/",
-      "icon": "http://img.kingsnug.cn/favicon.ico",
-      "name": "妮可",
-      "desc": "宅男腐女们最喜爱的动漫交流互动平台"
+      "url": "http://www.daqianduan.com/",
+      "icon": "http://www.daqianduan.com/favicon.ico",
+      "name": "大前端-关注前端开发",
+      "desc": "大前端是一个关注前端开发、用户体验设计、HTML5、CSS3、Javascript的前端开发独立博客。"
+    }]
+  }, {
+    "title": "前端",
+    "icon": "icon-router",
+    "item": [{
+      "tag": "教程",
+      "url": "https://standardjs.com/rules-zhcn.html#javascript-standard-style",
+      "icon": "https://standardjs.com/favicon/favicon.ico",
+      "name": "JavaScript Standard Style",
+      "desc": "JS Standard 规范"
+    }, {
+      "tag": "教程",
+      "url": "https://www.yuque.com/airing/canvas/readme",
+      "icon": "https://www.yuque.com/favicon.ico",
+      "name": "CANVAS教程",
+      "desc": "CANVAS基础教程"
+    }, {
+      "tag": "教程",
+      "url": "http://es6.ruanyifeng.com/",
+      "icon": "http://es6.ruanyifeng.com/favicon.ico",
+      "name": "ECMAScript 6 入门",
+      "desc": "《ECMAScript 6 入门》是一本开源的 JavaScript 语言教程，全面介绍 ECMAScript 6 新引入的语法特性。"
+    }, {
+      "tag": "博客",
+      "url": "https://www.zhangxinxu.com/",
+      "icon": "https://www.zhangxinxu.com/favicon.ico",
+      "name": "张鑫旭的个人主页",
+      "desc": "张鑫旭的个人博客首页，张鑫旭的技术作品，张鑫旭的生活成长"
+    }, {
+      "tag": "文章",
+      "url": "http://taobaofed.org/",
+      "icon": "http://taobaofed.org/favicon.ico",
+      "name": "Taobao FED | 淘宝前端团队",
+      "desc": "用技术为体验提供无限可能"
+    }, {
+      "tag": "教程",
+      "url": "https://wangdoc.com/javascript/",
+      "icon": "https://wangdoc.com/favicon.ico",
+      "name": "JavaScript 教程",
+      "desc": "本教程全面介绍 JavaScript 核心语法，从最简单的讲起，循序渐进、由浅入深，力求清晰易懂。所有章节都带有大量的代码实例，便于理解和模仿，可以用到实际项目中，即学即用。"
+    }, {
+      "tag": "社区",
+      "url": "http://www.jobbole.com/",
+      "icon": "http://www.jobbole.com/wp-content/uploads/2015/06/d02a42d9cb3dec9320e5f550278911c7.png",
+      "name": "伯乐在线",
+      "desc": "专业的IT互联网职业社区。"
+    }, {
+      "tag": "文章",
+      "url": "https://weekly.75team.com/",
+      "icon": "https://weekly.75team.com/favicon.ico",
+      "name": "奇舞周刊",
+      "desc": "我们收集每周前端精华文章，集结成册，每周五推送到微信公众号和知乎专栏。"
+    }, {
+      "tag": "英文 文章",
+      "url": "https://www.notion.so/0ba81e1707ae479b8c2b9ec79fe3a3ce",
+      "icon": "https://weekly.75team.com/favicon.ico",
+      "name": "前端英文网站汇总",
+      "desc": "前端英文网站汇总"
+    }, {
+      "tag": "CSS",
+      "url": "http://caibaojian.com/30-seconds-of-css/",
+      "icon": "http://caibaojian.com/30-seconds-of-css/a8de561a148aec60dd3697311c540b29.png",
+      "name": "30秒 CSS 代码片段精选",
+      "desc": "您可以在30秒或更短时间内理解的有用CSS片段的精选。从基本元素(如clearfix )到渐变文本颜色和渐变光标跟踪，再到CSS缓动等等。"
+    }, {
+      "tag": "框架资源",
+      "url": "https://www.awesomes.cn/",
+      "icon": "https://www.awesomes.cn/images/logo.png",
+      "name": "Awesomes",
+      "desc": "Web前端开发资源库"
+    }, {
+      "tag": "模拟 联系",
+      "url": "https://codepen.io/pens/",
+      "icon": "https://static.codepen.io/assets/favicon/logo-pin-f2d2b6d2c61838f7e76325261b7195c27224080bc099486ddd6dccb469b8e8e6.svg",
+      "name": "Explore Pens on CodePen",
+      "desc": "CodePen.io是一个浏览器内编码环境,专为学习编码和快速原型创作而设计,最大限度地减少麻烦。"
+    }, {
+      "tag": "工具",
+      "url": "https://caniuse.com/#home",
+      "icon": "https://caniuse.com/img/favicon-128.png",
+      "name": "Can I use...",
+      "desc": "查看技术兼容性"
+    }, {
+      "tag": "教程",
+      "url": "https://learn.freecodecamp.org/",
+      "icon": "https://learn.freecodecamp.org/assets/favicon-32x32.png",
+      "name": "freeCodeCamp",
+      "desc": "数以千计的编码课程来帮助您提高技能。"
+    }, {
+      "tag": "题库",
+      "url": "https://www.codewars.com/dashboard",
+      "icon": "https://www.codewars.com/favicon.png",
+      "name": "Codewars",
+      "desc": "代码解题的社区(支持多种编程语言),根据用户编码水平设置编程段位"
+    }, {
+      "tag": "题库",
+      "url": "https://leetcode-cn.com/",
+      "icon": "https://leetcode-cn.com/favicon.png",
+      "name": "力扣 LeetCode",
+      "desc": "备战技术面试？力扣提供海量技术面试资源，帮助你高效提升编程技能，轻松拿下世界 IT 名企 Dream Offer。"
+    }, {
+      "tag": "题库",
+      "url": "http://scriptoj.mangojuice.top/",
+      "icon": "http://scriptoj.mangojuice.top/favicon.ico",
+      "name": "ScriptOJ",
+      "desc": "Web前端实战代码、面试题目测试"
+    }, {
+      "url": "http://f2ex.cn/",
+      "icon": "http://f2ex.cn/favicon.ico",
+      "name": "F2EX",
+      "desc": "F2EX – web前端开发|web前端资源库",
+      "tag": "题库"
+    }, {
+      "url": "https://www.html.cn/",
+      "icon": "https://www.html.cn/favicon.ico",
+      "name": "html中文网",
+      "desc": "从html、css到 js，WEB前端开发在线学习视频教程 - html中文网",
+      "tag": "文章 教程"
+    }]
+  }, {
+    "title": "设计",
+    "icon": "icon-router",
+    "item": [{
+      "url": "https://weibo.com/u/3816118370/home?topnav=1&wvr=6",
+      "icon": "https://weibo.com/favicon.ico",
+      "name": "微博",
+      "desc": "随时随地发现新鲜事"
+    }]
+  }, {
+    "title": "系统",
+    "icon": "icon-router",
+    "item": [{
+      "url": "https://xinqiu.gitbooks.io/linux-insides-cn/content/index.html6",
+      "icon": "https://xinqiu.gitbooks.io/linux-insides-cn/content/gitbook/images/favicon.ico",
+      "name": "Linux内核揭秘",
+      "desc": "一系列关于 Linux 内核和其内在机理的帖子。"
+    }]
+  }, {
+    "title": "其他",
+    "icon": "icon-router",
+    "item": [{
+      "url": "https://weibo.com/u/3816118370/home?topnav=1&wvr=6",
+      "icon": "https://weibo.com/favicon.ico",
+      "name": "微博",
+      "desc": "随时随地发现新鲜事"
     }]
   }];
 });
