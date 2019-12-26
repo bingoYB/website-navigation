77815e6a36434a713ed0fff616cdb23b2667d6a6
lixs5 <lixs5@asiainfo.com>
2019-12-26 23:21:42 +0800

    更新

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,27 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin'); //通过 npm 安装
+const webpack = require('webpack'); //访问内置的插件
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     entry: {
         index: './src/script/index.js'
     },
+    mode:'production',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js'
     },
     plugins: [
-        new webpack.optimize.UglifyJsPlugin(),
-        new HtmlWebpackPlugin({ template: './public/index.html' })
+        // new webpack.optimize.UglifyJsPlugin(),
+        new HtmlWebpackPlugin({ template: './public/index.html' }),
+        new CopyWebpackPlugin([
+            {
+                from: path.resolve(__dirname, './public'),
+                to: path.resolve(__dirname, './dist'),
+                ignore: ['.html']
+            }
+        ])
     ]
     // module: {
     //     rules: [
