bd145ae1ebe578bf3dc6d2737c68b3fd6d9116fe
bingo <binhdu@foxmail.com>
2020-01-08 09:54:57 +0800

    删除非必要（旧）文件

--- dist/4.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{13:function(n,o){n.exports='<div class="my-nav"></div>'}}]);
--- webpack.config.js
+++ webpack.config.js
@@ -3,32 +3,36 @@ const HtmlWebpackPlugin = require('html-webpack-plugin'); //通过 npm 安装
 const webpack = require('webpack'); //访问内置的插件
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
-module.exports = {
-  entry: {
-    index: './src/script/index.js'
-  },
-  mode: 'production',
-  output: {
-    path: path.resolve(__dirname, 'docs'),
-    filename: '[name].js'
-  },
-  plugins: [
-    // new webpack.optimize.UglifyJsPlugin(),
-    new HtmlWebpackPlugin({ template: './public/index.html' }),
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, './public'),
-        to: path.resolve(__dirname, './docs'),
-        ignore: ['.html']
-      }
-    ])
-  ],
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.json$/,
-  //       use: 'json-loader'
-  //     }
-  //   ]
-  // }
+module.exports = function (env,args) {
+  console.log(env)
+  console.log(args)
+  return {
+    entry: {
+      index: './src/script/index.js'
+    },
+    mode: 'production',
+    output: {
+      path: path.resolve(__dirname, 'docs'),
+      filename: '[name].js'
+    },
+    plugins: [
+      // new webpack.optimize.UglifyJsPlugin(),
+      new HtmlWebpackPlugin({ template: './public/index.html' }),
+      new CopyWebpackPlugin([
+        {
+          from: path.resolve(__dirname, './public'),
+          to: path.resolve(__dirname, './docs'),
+          ignore: ['.html']
+        }
+      ])
+    ],
+    // module: {
+    //   rules: [
+    //     {
+    //       test: /\.json$/,
+    //       use: 'json-loader'
+    //     }
+    //   ]
+    // }
+  }
 }
