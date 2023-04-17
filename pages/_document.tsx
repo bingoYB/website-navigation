51e287262a5b962c71009b27ed51b5af1627b875
长空 <changkong@gaoding.com>
2023-04-17 13:53:04 +0800

    feat: 增加百度统计

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,20 +5,31 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel='icon' type='image/png' href='/img/favicon.png' />
+        <link rel="icon" type="image/png" href="/img/favicon.png" />
         <link
-          href='https://cdn.bootcss.com/normalize/8.0.1/normalize.min.css'
-          rel='stylesheet'
+          href="https://cdn.bootcss.com/normalize/8.0.1/normalize.min.css"
+          rel="stylesheet"
         ></link>
         {/* <!--阿里图标 --> */}
         <link
-          rel='stylesheet'
-          href='https://at.alicdn.com/t/font_1218740_b6bm55w3yga.css'
+          rel="stylesheet"
+          href="https://at.alicdn.com/t/font_1218740_b6bm55w3yga.css"
         />
-        <link rel='stylesheet' href='/style/pure.min.css' />
-        <link rel='stylesheet' href='/style/grids-responsive.min.css' />
-        <link rel='stylesheet' href='/style/nprogress.min.css' />
-        <link rel='stylesheet' href='/style/hint.min.css' />
+        <link rel="stylesheet" href="/style/pure.min.css" />
+        <link rel="stylesheet" href="/style/grids-responsive.min.css" />
+        <link rel="stylesheet" href="/style/nprogress.min.css" />
+        <link rel="stylesheet" href="/style/hint.min.css" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?bdcdc5e1bdfb8f469e0f1b4566c19c0c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();`,
+          }}
+        ></script>
       </Head>
       <body>
         <Main />
